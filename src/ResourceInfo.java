5b72e03ced901c19dec0396ff12d35f51753ed6f
Karbust <charparodar@gmail.com>
2021-02-24 21:48:54 +0000

    Final

---
+++ src/ResourceInfo.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class ResourceInfo implements Serializable {
+    public String resourceType;
+    public Float resourceTime;
+    public String cachedResource;
+    public Integer harRun;
+    public long resourceLength;
+}
